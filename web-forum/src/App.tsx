90822cde761bf08a9263e2f14ec429456190f6d3
Deni Denchev <d.d.denchev94@gmail.com>
2025-05-19 00:53:45 +0300

    init

--- web-forum/src/App.tsx
+++ web-forum/src/App.tsx
@@ -7,29 +7,34 @@ import LandingPage from "./pages/LandingPage";
 import CategoriesPage from "./pages/CategoriesPage";
 import TopicsPage from "./pages/TopicsPage";
 import TopicDetailPage from "./pages/TopicDetailPage";
-
-
-import NavBar from "./components/NavBar";
-import { ThemeContextProvider } from "./context/ThemeContext";
 import ConversationPage from "./components/ConversationPage";
+import NavBar from "./components/NavBar";
+
+import { ThemeProvider } from "./context/ThemeContext";
+import Footer from "./footer/Footer";
 
 export default function App() {
   return (
-    <ThemeContextProvider>
+    <ThemeProvider>
       <CssBaseline />
       <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/messages/with/:username" element={<ConversationPage />} />
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/categories" element={<CategoriesPage />} />
-          <Route path="/topics" element={<TopicsPage />} />
-          <Route path="/topic-detail" element={<TopicDetailPage />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <div className="flex flex-col min-h-screen">
+          <NavBar />
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/categories" element={<CategoriesPage />} />
+              <Route path="/topics" element={<TopicsPage />} />
+              <Route path="/topic-detail" element={<TopicDetailPage />} />
+              <Route path="/messages/with/:username" element={<ConversationPage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
       </Router>
-    </ThemeContextProvider>
+    </ThemeProvider>
   );
 }
